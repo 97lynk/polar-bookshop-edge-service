826034c17e9d3dbfb5cdb53ce9e0c3763e4f027e
Tuan Nguyen <tuananh@gearment.com>
2025-04-13 01:32:53 +0700

    Chapter 16: - add config for acceptance stage Github Action

--- src/main/java/com/polarbookshop/edgeservice/web/WebEndpoints.java
+++ src/main/java/com/polarbookshop/edgeservice/web/WebEndpoints.java
@@ -1,5 +1,6 @@
 package com.polarbookshop.edgeservice.web;
 
+import com.polarbookshop.edgeservice.config.PolarProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
@@ -12,8 +13,10 @@ import reactor.core.publisher.Mono;
 public class WebEndpoints {
 
     @Bean
-    public RouterFunction<ServerResponse> routerFunction() {
+    public RouterFunction<ServerResponse> routerFunction(PolarProperties polarProperties) {
         return RouterFunctions.route()
+                .GET("/hello", request ->
+                        ServerResponse.ok().body(Mono.just(polarProperties.getGreeting()), String.class))
                 .GET("/catalog-fallback", request -> {
                     System.out.println(request);
                     return ServerResponse.ok().body(Mono.just("[]"), String.class);
